9900de85e8288666ba3c1c5702bd6afa5b69fb44
sebastianiscruleasa <sebi.iscruleasa@yahoo.ro>
2023-02-22 20:54:00 +0200

    added attention unet in net factory

--- code/networks/net_factory.py
+++ code/networks/net_factory.py
@@ -4,6 +4,7 @@ from networks.unet import UNet_DS, UNet_URPC, UNet_CCT
 #from networks.unet import UNet
 from networks.myUnet import UNet
 from networks.R2UNet import R2UNet
+from networks.AttentionUnet import AttentionUnet
 from networks.efficientunet import Effi_UNet
 from networks.enet import ENet
 from networks.pnet import PNet2D
@@ -82,8 +83,8 @@ def net_factory(net_type="unet", in_chns=1, class_num=3):
         net = UNet(in_channels=in_chns, out_channels=class_num).to(device)
     elif net_type == "r2unet":
         net = R2UNet(in_channels=in_chns, out_channels=class_num).to(device)
-    elif net_type == "unet_ds":
-        net = UNet_DS(in_chns=in_chns, class_num=class_num).cuda()
+    elif net_type == "attunet":
+        net = AttentionUnet(in_channels=in_chns, out_channels=class_num).to(device)
     elif net_type == "unet_cct":
         net = UNet_CCT(in_chns=in_chns, class_num=class_num).cuda()
     elif net_type == "unet_urpc":
