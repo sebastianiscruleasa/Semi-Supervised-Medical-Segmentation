32441f2a4794f73086b4d12c5b9089d8e371a4f9
sebastianiscruleasa <sebi.iscruleasa@yahoo.ro>
2023-02-23 20:00:41 +0200

    improved R2Unet for better readability

--- code/networks/R2UNet.py
+++ code/networks/R2UNet.py
@@ -20,9 +20,9 @@ class RecurrentBlock(nn.Module):
         return previous_state
 
 
-class RecurrentResidualBlock(nn.Module):
+class RecurrentResidualConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, timestamps=2):
-        super(RecurrentResidualBlock, self).__init__()
+        super(RecurrentResidualConvBlock, self).__init__()
         self.conv1x1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
         self.layers = nn.Sequential(
             RecurrentBlock(out_channels, timestamps),
@@ -40,7 +40,7 @@ class RecurrentResidualDownBlock(nn.Module):
         super(RecurrentResidualDownBlock, self).__init__()
         self.layers = nn.Sequential(
             nn.MaxPool2d(2),
-            RecurrentResidualBlock(in_channels, out_channels, timestamps),
+            RecurrentResidualConvBlock(in_channels, out_channels, timestamps),
         )
 
     def forward(self, x):
@@ -50,38 +50,50 @@ class RecurrentResidualDownBlock(nn.Module):
 class R2UNet(nn.Module):
     def __init__(self, in_channels, out_channels, timestamps=2):
         super(R2UNet, self).__init__()
-        self.conv1 = RecurrentResidualBlock(in_channels, 64, timestamps)
+        self.conv1 = RecurrentResidualConvBlock(in_channels, 64, timestamps)
+
         self.down1 = RecurrentResidualDownBlock(64, 128, timestamps)
         self.down2 = RecurrentResidualDownBlock(128, 256, timestamps)
         self.down3 = RecurrentResidualDownBlock(256, 512, timestamps)
         self.down4 = RecurrentResidualDownBlock(512, 1024, timestamps)
+
         self.up1 = UpBlock(1024, 512)
-        self.conv2 = RecurrentResidualBlock(1024, 512)
+        self.conv2 = RecurrentResidualConvBlock(1024, 512)
+
         self.up2 = UpBlock(512, 256)
-        self.conv3 = RecurrentResidualBlock(512, 256)
+        self.conv3 = RecurrentResidualConvBlock(512, 256)
+
         self.up3 = UpBlock(256, 128)
-        self.conv4 = RecurrentResidualBlock(256, 128)
+        self.conv4 = RecurrentResidualConvBlock(256, 128)
+
         self.up4 = UpBlock(128, 64)
-        self.conv5 = RecurrentResidualBlock(128, 64)
+        self.conv5 = RecurrentResidualConvBlock(128, 64)
+
         self.out = nn.Conv2d(64, out_channels, kernel_size=1)
 
     def forward(self, x):
-        x1 = self.conv1(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5)
-        x = torch.cat([x, x4], dim=1)
-        x = self.conv2(x)
-        x = self.up2(x)
-        x = torch.cat([x, x3], dim=1)
-        x = self.conv3(x)
-        x = self.up3(x)
-        x = torch.cat([x, x2], dim=1)
-        x = self.conv4(x)
-        x = self.up4(x)
-        x = torch.cat([x, x1], dim=1)
-        x = self.conv5(x)
-        x = self.out(x)
-        return x
+        encoder1 = self.conv1(x)
+
+        encoder2 = self.down1(encoder1)
+        encoder3 = self.down2(encoder2)
+        encoder4 = self.down3(encoder3)
+        encoder5 = self.down4(encoder4)
+
+        decoder5 = self.up1(encoder5)
+        decoder5 = torch.cat([encoder4, decoder5], dim=1)
+        decoder5 = self.conv2(decoder5)
+
+        decoder4 = self.up2(decoder5)
+        decoder4 = torch.cat([encoder3, decoder4], dim=1)
+        decoder4 = self.conv3(decoder4)
+
+        decoder3 = self.up3(decoder4)
+        decoder3 = torch.cat([encoder2, decoder3], dim=1)
+        decoder3 = self.conv4(decoder3)
+
+        decoder2 = self.up4(decoder3)
+        decoder2 = torch.cat([encoder1, decoder2], dim=1)
+        decoder2 = self.conv5(decoder2)
+
+        decoder1 = self.out(decoder2)
+        return decoder1
